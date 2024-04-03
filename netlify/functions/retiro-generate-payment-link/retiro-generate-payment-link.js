4fcb3c8b9af8a240024d6a37dd9f1358a9395f20
Luiz Venturote <luiz@imdigital.com>
2024-04-03 19:39:22 -0300

    Update retiro pages

--- netlify/functions/retiro-generate-payment-link/retiro-generate-payment-link.js
+++ netlify/functions/retiro-generate-payment-link/retiro-generate-payment-link.js
@@ -1,5 +1,5 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event) => {
+const handler = async (e) => {
   try {
 
     // Get request id, name and age param
