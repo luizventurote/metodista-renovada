d7f831666d31726641c231c1d74dab1b38c6a6ca
Luiz Venturote <luiz@imdigital.com>
2024-05-26 14:25:17 -0300

    Add asaas generate payment link

---
+++ netlify/functions/asaas-generate-payment-link/asaas-generate-payment-link.js
@@ -0,0 +1,48 @@
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const handler = async (event) => {
+    try {
+  
+      var myHeaders = new Headers();
+      myHeaders.append("access_token", ASAAS_API_KEY);
+      myHeaders.append("Content-Type", "application/json");
+  
+      var raw = JSON.stringify({
+        "billingType": "UNDEFINED",
+        "chargeType": "INSTALLMENT",
+        "maxInstallmentCount": 1,
+        "callback": {
+          "autoRedirect": true,
+          "successUrl": "https://www.metodistarenovada.com/oferta?pagamento=confirmado"
+        },
+        "name": "Doação para a construção da nova sede da Igreja Metodista Renovada de Colatina",
+        "description": "Faça a sua doação para ajudar na construção da nova sede da Igreja Metodista Renovada de Colatina. \"O tamanho da fé é o que define o tamanho de uma igreja, pois nosso limite de expansão depende do quanto acreditamos\"",
+        "notificationEnabled": false,
+        "dueDateLimitDays": 3
+      });
+  
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+  
+      return fetch("https://api.asaas.com/v3/paymentLinks", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+              return {
+                statusCode: 200,
+                body: JSON.stringify({ result }),
+                // // more keys you can return:
+                // headers: { "headerName": "headerValue", ... },
+                // isBase64Encoded: true,
+              }
+            })
+            .catch(error => console.log('error', error));
+    } catch (error) {
+      return { statusCode: 500, body: error.toString() }
+    }
+  }
+  
+  module.exports = { handler }
+  
